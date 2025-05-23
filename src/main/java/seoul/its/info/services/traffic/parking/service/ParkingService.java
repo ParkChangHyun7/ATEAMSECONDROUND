61593590ceb04e9e950f7e04e19bacd7a155b932
ParkChangHyun777 <changhyun19951003@gmail.com>
2025-05-23 09:23:54 +0900

    ParkingService.java수정

--- src/main/java/seoul/its/info/services/traffic/parking/service/ParkingService.java
+++ src/main/java/seoul/its/info/services/traffic/parking/service/ParkingService.java
@@ -1,13 +1,16 @@
-//ParkingService.java
-//주차장 정보를 가져오는 기능의 인터페이스
 package seoul.its.info.services.traffic.parking.service;
 
+import seoul.its.info.services.traffic.parking.dto.PublicParkingDto;
 import java.util.List;
-import seoul.its.info.services.traffic.parking.dto.ParkingDto;
-
+/**
+ * 주차장 관련 서비스 로직을 정의한 인터페이스
+ */
 
 public interface ParkingService {
 
-    //주차장 목록을 전체 조회하는 메서드
-    List<ParkingDto>getAllParkingLots();
+    /**
+     * 공영 주차장 목록 전체를 반환하는 메서드
+     * @return PublicParkingDto리스트
+     */
+    List<PublicParkingDto>getAllParkingLots();
 }
