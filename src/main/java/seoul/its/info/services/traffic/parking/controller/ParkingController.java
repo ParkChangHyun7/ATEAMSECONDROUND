6b58dd2861580c85663ffe69c2e480c1d209ee80
ParkChangHyun777 <changhyun19951003@gmail.com>
2025-05-23 18:02:40 +0900

    여러 파일 수정

--- src/main/java/seoul/its/info/services/traffic/parking/controller/ParkingController.java
+++ src/main/java/seoul/its/info/services/traffic/parking/controller/ParkingController.java
@@ -1,41 +1,33 @@
 package seoul.its.info.services.traffic.parking.controller;
 
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import seoul.its.info.services.traffic.parking.dto.PublicParkingDto;
-import seoul.its.info.services.traffic.parking.service.ParkingService;
-
-import java.util.List;
 
 
 /**
- * JSON 데이터 제공용 RestController
- * -클라이언트(Vue, JS등)에서 fetch, axios 요청 보낼 때 사용하는 API전용 컨트롤러
- * -화면(JSP)을 띄우는 역할은 하지 않음
+ * 주차장 페이지(JSP)진입용 Controller
+ * 이 컨트롤러는 브라우저에서 URL을 입력했을 때 JSP화면을 보여주는 역할
+ * JSON데이터를 응답한는게 아니라 JSP화면을 리턴하는게 목적
  */
 
- @RestController
- @RequestMapping("/api") //모든 요청의 앞부분은 /api로 시작
- public class ParkingController{
-
-    private final ParkingService parkingService;
-
-    //생성자 주입 방식 사용
-    public ParkingController(ParkingService parkingService){
-        this.parkingService = parkingService;
-    }
+@Controller
+public class ParkingPageController {
 
     /**
-     * GET요청: /api/parking
-     * -전체 공영 주차장 데이터를 JSON 배열 형태로 응답
-     * -fetch()로 지도에 마커 찍을 때 이 데이터를 사용함
+     * 최초 진입 URL: http://localhost:9998/parking
+     * -Jsp 화면을 띄우기 위한 요청
+     * model에 contentPage값을 넣어 base.jsp가 해당 JSP를 include하도록 함
      * 
-     * @return 주차장 목록 List<PublicParkingDto>
+     * @param model -JSP include 대상 설정을 위핸 Model객체
+     * @return base.jsp로 이동하여 ,contentPage속성에 따라 실제 화면 로딩
      */
-     @GetMapping("/parking")
-     public List<PublicParkingDto>getAllParkingLots(){
-        return parkingService.getAllPublicParking();
-     }
- }
+    @GetMapping("/parking")
+    public String parkingPage(Model model ) {
+        //base.jsp에서 <jsp:include page="${contentPage}"/>형태로 사용될 경로
+        model.addAttribute("contentPage", "content_pages/parking/parking.jsp");
+
+        //base.jsp안에 위에서 설정한 contentPage를 include
+        return "base";// => views/base.jsp
+    }
+}
