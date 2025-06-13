18aba34585cd13a50ca4582a4b0b66336f5611ed
ParkChangHyun777 <changhyun19951003@gmail.com>
2025-06-13 16:54:05 +0900

    1

--- src/main/java/seoul/its/info/services/AirQualityProxy/AirQualityProxyController.java
+++ src/main/java/seoul/its/info/services/AirQualityProxy/AirQualityProxyController.java
@@ -1,5 +1,6 @@
 package seoul.its.info.services.AirQualityProxy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
@@ -10,10 +11,11 @@ import java.util.Map;
 @RequestMapping("/api/air")
 public class AirQualityProxyController {
 
+    @Value("${open.api.base_key}")
+    private String apiKey;
+
     @GetMapping
     public ResponseEntity<Map> getAirQuality() {
-        // 발급받은 키 중 아무거나 사용 가능
-        String apiKey = "466b566d6f636861343879484c5773";
         String url = "http://openapi.seoul.go.kr:8088/" + apiKey + "/json/RealtimeCityAir/1/25/";
 
         RestTemplate restTemplate = new RestTemplate();
--- src/main/webapp/WEB-INF/views/include/AirQualityProxy/scripts.jsp
+++
@@ -1,6 +0,0 @@
-<script>
-  // 💡 window 전역에 키 등록
-  window.API_KEYS = {
-    SEOUL_API_KEY: "466b566d6f636861343879484c5773"
-  };
-</script>
